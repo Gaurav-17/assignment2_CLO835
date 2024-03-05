9a7e29dc682c042270f3600229b452f6ff46fd10
Gaurav-17 <64305413+Gaurav-17@users.noreply.github.com>
2024-03-05 02:18:43 +0000

    modified ec2.tf file to launch kind cluster with right key configurtaions

--- terraform_ass2/ec2.tf
+++ terraform_ass2/ec2.tf
@@ -45,13 +45,14 @@ resource "aws_instance" "k8s" {
     project = "clo835"
   }
 
-  key_name                = "week5"
+  key_name                = "ass2"
   monitoring              = true
   disable_api_termination = false
   ebs_optimized           = true
 }
 
+
 resource "aws_key_pair" "k8s" {
-  key_name   = "week5"
-  public_key = file("${path.module}/week5.pub")
+  key_name   = "ass2"
+  public_key = file("${path.module}/ass2.pub")
 }
